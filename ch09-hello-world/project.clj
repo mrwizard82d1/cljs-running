d472dd137599893583c221b63c0ac9e4cfd6b53d
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-03-08 16:08:44 +0000

    Copied chapter 2 files to compile manually. Removed .gitignore files.

---
+++ ch09-hello-world/project.clj
@@ -0,0 +1,15 @@
+(defproject hello-world "0.1.0-SNAPSHOT"
+  :description "'Hello, World' JavaScript app from 'ClojureScript - Up and Running.'"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :source-paths ["src/clj"]
+  :cljsbuild {:builds
+              [{:source-paths ["src/cljs"],
+                :compiler
+                {:pretty-print true,
+                 :output-to "resources/public/hello.js",
+                 :optimizations :whitespace}}]})
